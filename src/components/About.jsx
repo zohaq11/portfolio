bea20e8f985c17340d84925a97f87c807d18055b
qamarzoha@hotmail.com <qamarzoha@hotmail.com>
2025-07-21 21:26:48 -0400

    add google analytics

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,7 +5,7 @@ import styles from '../style'
 const About = () => {
   return (
     <div className="flex flex-col md:flex-row items-start md:items-start justify-start gap-8 md:gap-16 p-8 min-h-screen">
-      {/* Left side: text */}
+ 
     <div className="md:w-1/2 w-full mb-8 md:mb-0">
     <h1 className="font-pixel font-bold ss:text-[72px] text-[52px] text-purple ss:leading-[100.8px] leading-[75px]">About Me</h1>
     <p className="font-pixel text-lg text-white leading-relaxed"> Hi! I'm a recent UofT CS and Stats grad and this is my portfolio website😁
@@ -13,7 +13,6 @@ const About = () => {
     <br /><br /> Heres to more projects where I get to combine my technical skills with creativity so stay tuned for updates as I have so many ideas cooking and so much more to add here🙂‍↕️</p>
     </div>
 
-    {/* Right side: icon/image */}
       <div className="md:w-1/2 w-full flex justify-center">
         <img 
           src={pic} 
