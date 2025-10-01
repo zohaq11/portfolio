bec39bc55f905f73c9b57d89fbb81a59ed337faa
qamarzoha@hotmail.com <qamarzoha@hotmail.com>
2025-09-30 23:59:37 -0400

    update

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,7 +8,7 @@ const About = () => {
  
     <div className="md:w-1/2 w-full mb-8 md:mb-0">
     <h1 className="font-pixel font-bold ss:text-[72px] text-[52px] text-purple ss:leading-[100.8px] leading-[75px]">About Me</h1>
-    <p className="font-pixel text-lg text-white leading-relaxed"> Hi! I'm a recent UofT CS and Stats grad and this is my portfolio website😁
+    <p className="font-pixel text-lg text-white leading-relaxed"> Hi! I'm a HBSc 2025 graduate from UofT that studied CS and Stats and this is my portfolio website😁
     <br /><br /> Since a portfolio is supposed to showcase my work, I figured what better way than digitalizing where the magic happens🪄 - my desk!
     <br /><br /> Heres to more projects where I get to combine my technical skills with creativity so stay tuned for updates as I have so many ideas cooking and so much more to add here🙂‍↕️</p>
     </div>
