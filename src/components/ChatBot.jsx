4ba3cd97a45638bd81026800fd0daa0dbb8e1079
qamarzoha@hotmail.com <qamarzoha@hotmail.com>
2026-05-16 01:49:49 -0400

    refactor'

--- src/components/ChatBot.jsx
+++ src/components/ChatBot.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useRef, useEffect } from 'react';
 import axios from 'axios';
-import zohaInfo from '../zohaInfo.json';
 
 const ChatBot = () => {
   const [isOpen, setIsOpen] = useState(false);
